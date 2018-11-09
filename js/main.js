75f2cd58192ae8103b6e1e65f8608b065b2a2d29
PlasticShitlord <40578297+PlasticShitlord@users.noreply.github.com>
2018-11-09 11:41:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,23 +6,46 @@ var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+
+    // Output the result in an element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
-    
-    // If the count down is over, write some text 
+
+    // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("countdown").innerHTML = "EXPIRED";
     }
 }, 1000);
+
+
+var target = document.getElementById('countdown-title');
+var titles = [
+    "Avocato will not come back",
+    "Body count starts",
+    "You'll be legally allowed to hate Lord Commander",
+    "You will lose it",
+    "H.U.E. will reboot",
+    "Mooncake's laser will be fully charged",
+    "Gary grows his mustache",
+    "The cookies will bake again",
+    "All bets are off",
+    "KVN will reboot",
+    "Another sadness deposit"
+];
+
+function newTitle () {
+    var i = (Math.random() * titles.length) | 0;
+    target.innerText = titles[i];
+}
+
+newTitle();
