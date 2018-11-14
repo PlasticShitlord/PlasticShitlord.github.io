678ba27a99ea9fb75fb9640de406284298367f3e
PlasticShitlord <amatiel98@mail.ru>
2018-11-14 11:50:55 +0300

    lined that up a little bit

--- js/countdown.js
+++ js/countdown.js
@@ -12,8 +12,7 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     // Output the result in an element with id="countdown"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
