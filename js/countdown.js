9df977c916a8d3003d3e32c29234e1eaa53bad91
PlasticShitlord <amatiel98@mail.ru>
2018-11-16 12:55:40 +0300

    gmt update

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("2019-03-05 01:00:00 GMT+0400").getTime();//moscow//
+var countDownDate = new Date("2019-03-05 01:00:00 GMT").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
     // Get todays date and time
